378d258a43870fea8fd97997a37760ba46b873b9
DeeeevDive <141470292+dev-diver@users.noreply.github.com>
2024-01-02 19:20:49 +0900

    second commit

---
+++ src/test/java/com/gang/mypage/MypageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gang.mypage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
